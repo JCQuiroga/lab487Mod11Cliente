ed711b25212ae218d541db0b1fc4b6e735d30c89
Jose C Quiroga <josecquiroga@gmail.com>
2016-02-23 12:32:30 +0100

    -═╬═-

--- Lab487Mod11Cliente/MainWindow.xaml.cs
+++ Lab487Mod11Cliente/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,19 +28,75 @@ namespace Lab487Mod11Cliente
         {
             InitializeComponent();
         }
-        
+
+        private AuthenticationResult auth;
+
         private AuthenticationResult Autenticar()
         {
-            var ctx = new AuthenticationContext("http://login.windows.net/lab487mod11ad.onmicrosoft.com");
+            var ctx = new AuthenticationContext("https://login.windows.net/lab487mod11ad.onmicrosoft.com");
 
             var res = ctx.AcquireToken("https://lab487mod11ad.onmicrosoft.com/Lab487Mod11", "3ca5457d-207f-464e-ac33-38f4c49538b2", "http://midominio/ok");
 
             return res;
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private async Task<string> Operar(AuthenticationResult auth, string op, Operacion operacion)
         {
+            var res = string.Empty;
 
+            HttpClient cl = new HttpClient();
+            cl.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", auth.AccessToken);
+
+            var response = await cl.PostAsync("https://lab487mod11appservice.azurewebsites.net/api/" + op, new StringContent(operacion.ToJson(), Encoding.UTF8, "application/json"));
+
+            if (response.IsSuccessStatusCode) res = await response.Content.ReadAsStringAsync();
+
+            return res;
+
+        }
+
+        private async void suma(object sender, RoutedEventArgs e)
+        {
+            var op = new Operacion()
+            {
+                Op1 = Convert.ToInt32(textBox.Text),
+                Op2 = Convert.ToInt32(textBox1.Text)
+            };
+            if(auth == null) auth = Autenticar();
+            label.Content = await Operar(auth,"suma",op);
+        }
+
+        private async void resta(object sender, RoutedEventArgs e)
+        {
+            var op = new Operacion()
+            {
+                Op1 = Convert.ToInt32(textBox.Text),
+                Op2 = Convert.ToInt32(textBox1.Text)
+            };
+            if (auth == null) auth = Autenticar();
+            label.Content = await Operar(auth, "resta", op);
+        }
+
+        private async void producto(object sender, RoutedEventArgs e)
+        {
+            var op = new Operacion()
+            {
+                Op1 = Convert.ToInt32(textBox.Text),
+                Op2 = Convert.ToInt32(textBox1.Text)
+            };
+            if (auth == null) auth = Autenticar();
+            label.Content = await Operar(auth, "producto", op);
+        }
+
+        private async void division(object sender, RoutedEventArgs e)
+        {
+            var op = new Operacion()
+            {
+                Op1 = Convert.ToInt32(textBox.Text),
+                Op2 = Convert.ToInt32(textBox1.Text)
+            };
+            if (auth == null) auth = Autenticar();
+            label.Content = await Operar(auth, "division", op);
         }
     }
 }
---
+++ Lab487Mod11Cliente/Operacion.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Script.Serialization;
+
+namespace Lab487Mod11Cliente
+{
+    public class Operacion
+    {
+        public int Op1 { get; set; }
+        public int Op2 { get; set; }
+
+        public string ToJson()
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+
+            return ser.Serialize(this);
+        }
+
+
+    }
+}
