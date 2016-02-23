ae38e5eb58482254355196714a3d6e85796edca2
Jose C Quiroga <josecquiroga@gmail.com>
2016-02-23 11:16:17 +0100

    no message

--- Lab487Mod11Cliente/MainWindow.xaml.cs
+++ Lab487Mod11Cliente/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAzure.ActiveDirectory.Authentication;
 
 namespace Lab487Mod11Cliente
 {
@@ -24,8 +26,17 @@ namespace Lab487Mod11Cliente
         {
             InitializeComponent();
         }
+        
+        private AuthenticationResult Autenticar()
+        {
+            var ctx = new AuthenticationContext("http://login.windows.net/lab487mod11ad.onmicrosoft.com");
 
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+            var res = ctx.AcquireToken("https://lab487mod11ad.onmicrosoft.com/Lab487Mod11", "3ca5457d-207f-464e-ac33-38f4c49538b2", "http://midominio/ok");
+
+            return res;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
         {
 
         }
